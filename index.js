b274b2272873398b26e38875595ae2efc3db1e1e
Chase <chaseit12@gmail.com>
2018-04-03 21:34:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ function BoardMember(name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
+  
+  veto: function(){
+    return 'No, I must disagree'
+  }
+  
+  
 }
